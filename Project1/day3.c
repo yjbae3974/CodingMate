58ca068b49d07a78261486e31c1fcaf637e5bc59
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-10-09 00:19:02 +0900

    day3-숫자 2개를 '문자열'로 입력받고 두 수 사이에 원하는 숫자의 배수를 있는대로 출력.

--- Project1/day3.c
+++ Project1/day3.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-
+#include <string.h>	//strtok 함수를 불러오기 위한 헤더
+#include<stdlib.h>	//atoi 함수를 불러오기 위한 헤더
 //공백으로 구분된 문자열 형태의 두 숫자를 입력받고, n을 입력받아 두 숫자 사이에 존재하는 n의
 //배수를 구해보시오.
 //조건1: 숫자 두개는 공백(스페이스 바)로 구분되어 입력됩니다.
@@ -26,28 +27,23 @@ int findMax(int a, int b) {
 }
 
 int main() {
-	//github commit 실험용
+	char s[20];		//공백을 포함한 문자열을 받기 위한 변수. 공간은 임의로 20으로 설정함.
+	char* s1 = NULL;
+	char* s2 = NULL;
+	char* tmp = NULL;
 	int a, b;
 	int times;
 	int init;
 	printf("숫자 두 개를 입력해주세요(ex. '3 5'):");
-	scanf("%c %c", &a, &b);
+	scanf("%[^\n]s", s);	//이로 인해 공백을 포함한 문자열을 입력받음.
+	s1 = strtok(s, " ");	//공백을 null문자로 치환하여 문자열의 끝으로 인식하도록 함. 이때 s도 바뀜.
+	s2 = strtok(NULL, " ");
+	a = atoi(s1);
+	b = atoi(s2);
 	printf("배수를 알고 싶은 숫자를 입력해주세요:");
 	scanf("%d", &times);
 	int min = findMin(a, b);
 	int max = findMax(a, b);
-	//for (int i = 0;;i++) {
-	//	if (((min + i) % times) == 0) {
-	//		init = min + i;
-	//		break;
-	//	}
-	//	else {
-	//		continue;
-	//	}
-	//}
-	//for (int i = init; i <= max; i += times) {
-	//	printf("%d ", i);
-	//}
 	for (int i = min; i <= max; i++) {
 		if (i % times == 0) {
 			printf("%d ", i);
